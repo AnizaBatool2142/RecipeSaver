e8df5a46fb6c1dd2c277421055a71144df067654
AnizaBatool2142 <aniza.batool@o3interfaces.com>
2024-11-26 20:43:54 +0500

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by ANEEZA on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
